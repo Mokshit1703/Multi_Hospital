264db220b9b2e080b523866ce7596f3d828a8ea2
Mokshit <mokshit.dww@gmail.com>
2020-09-11 16:07:41 +0530

    Completed

---
+++ dompdf2.php
@@ -0,0 +1,16 @@
+<?php
+
+//iclude autoload
+require_once 'dompdf/autoload.inc.php';
+
+use Dompdf\Dompdf;
+$document = new Dompdf();
+
+$page = file_get_contents('viewpatientdetails.html');
+
+$document->loadHtml($page);
+$document->setPaper('A4','landscape');
+$document->render();
+$document->stream("weblession",array("Attachment"=>0));
+
+?>
